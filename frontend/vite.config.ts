f05f7b467f3df2d7626ba3c679e1edfa5bfe7638
Sulkhanov Ilya <masta.pxty@gmail.com>
2026-01-21 23:56:55 +0700

    feat: add fodler aliases

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,4 +16,15 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@/app': path.resolve(__dirname, './src/app'),
+      '@/pages': path.resolve(__dirname, './src/pages'),
+      '@/widgets': path.resolve(__dirname, './src/widgets'),
+      '@/features': path.resolve(__dirname, './src/features'),
+      '@/entities': path.resolve(__dirname, './src/entities'),
+      '@/shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
 });
